d0b913d1010b0590b4b5542bfc5a4cbb5d0a546b
hyennin <chm050202@gmail.com>
2023-05-18 20:05:29 +0900

    component 분리, map 사용해서 반복

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+import bg from './img/bg1.png';
+import shoes01 from './img/shoes01.png'
+import shoes02 from './img/shoes02.png'
+import shoes03 from './img/shoes03.png'
+import data from './data.js';
 
 function App() {
+  const [shoes] = useState(data);
+  const shoesImg = [shoes01, shoes02, shoes03];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <Button variant="success">Success</Button> */}
+        <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">ShoesShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      
+      <div className='main-bg' style={{backgroundImage : 'url('+bg+')'}}></div>
+      <div className='container'>
+        <div className='row'>
+          { shoes.map((s, idx) => <Card image={shoesImg[idx]} title={s.title} content={s.content}/> )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const Card = (props) => {
+  return (
+    <div className='col-md-4'>
+      <img src={props.image} alt='에어'/>
+      <h4>{props.title}</h4>
+      <p>{props.content}</p> 
     </div>
   );
 }
