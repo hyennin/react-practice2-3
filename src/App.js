87538e0f591422e86a37c8efc2f53c9698940071
hyennin <chm050202@gmail.com>
2023-05-23 19:40:56 +0900

    router 실습

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { useState } from 'react';
-import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
 import bg from './img/bg1.png';
 import shoes01 from './img/shoes01.png'
 import shoes02 from './img/shoes02.png'
 import shoes03 from './img/shoes03.png'
 import data from './data.js';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   const [shoes] = useState(data);
@@ -22,19 +20,24 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">ShoesShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Link to='/'>홈</Link>
+            <Link to='/detail'>상세페이지</Link>
           </Nav>
         </Container>
       </Navbar>
-      
-      <div className='main-bg' style={{backgroundImage : 'url('+bg+')'}}></div>
-      <div className='container'>
-        <div className='row'>
-          { shoes.map((s, idx) => <Card image={images[idx]} shoes={s}/> )}
-        </div>
-      </div>
+      <Routes>
+        <Route path='//' element={
+          <>
+            <div className='main-bg' style={{backgroundImage : 'url('+bg+')'}}></div>
+            <div className='container'>
+              <div className='row'>
+                { shoes.map((s, idx) => <Card image={images[idx]} shoes={s}/> )}
+              </div>
+            </div>
+          </>
+        }/>
+        <Route path='/detail' element={<div>detail페이지입니다.</div>}/>
+      </Routes>
     </div>
   );
 }
