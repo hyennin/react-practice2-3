17133f6d201f4f3ecec04b8c12a610d0b9e31b88
hyennin <chm050202@gmail.com>
2023-05-25 19:36:24 +0900

    detail 실습

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,6 @@
 // import { useParams } from "react-router-dom";
 
-const Detail = () => {
+const Detail = (props) => {
     return (
         <div className="container">
             <div className="row">
@@ -8,9 +8,9 @@ const Detail = () => {
                     <img src={'https://raw.githubusercontent.com/hyennin/react-practice2-3/master/src/img/shoes01.png' } width="100%" alt="에어"/>
                 </div>
                 <div className="col-md-6">
-                    <h4 className="pt-5">상품명</h4>
-                    <p>상품설명</p>
-                    <p>120000원</p>
+                    <h4 className="pt-5">{props.shoes[0].title}</h4>
+                    <p>{props.shoes[0].content}</p>
+                    <p>{props.shoes[0].price}</p>
                     <button className="btn btn-danger">주문하기</button> 
                 </div>
             </div>
