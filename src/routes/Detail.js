65b16cf3d5b5e46a485b76c833958bac15b8d753
hyennin <chm050202@gmail.com>
2023-05-30 20:09:00 +0900

    useEffect 실습

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,3 +1,14 @@
+/*
+컴포넌트의 Life Cycle
+1. 페이지 로딩(mount)
+2. 업데이트(update)
+3. 제거(unmount)
+
+mount, update 시 코드 실행하려면
+useEffect() 사용
+*/
+
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -17,11 +28,22 @@ const Box = styled.div`
 `;
 
 const Detail = (props) => {
+    let [ alert, setAlert ] = useState(true);
+    
+    useEffect(() => {
+        let a = setTimeout(() => setAlert(false), 2000);
+        return () => {
+            // 타이머 제거, useEffect가 실행되기 전에 실행됨
+            clearTimeout(a);
+        }
+    }, []) // [] : 첫 마운트 때에만 실행
+
     // 유저가 입력한 URL 파라미터의 값을 읽어오기 위해 useParams()
     let { id } = useParams();
 
     return (
         <div className="container">
+            { alert ? <div className="alert alert-waring">2초 이내 구매 시 할인</div> : '' }
             <Box>
                 <YellowBtn bg="blue">스타일버튼_blue</YellowBtn>
                 <NewBtn bg="green">스타일버튼_green</NewBtn>
