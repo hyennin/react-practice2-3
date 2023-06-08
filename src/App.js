9f1f841233a79567896c584e0b6a375202fe240e
hyennin <chm050202@gmail.com>
2023-06-08 19:44:57 +0900

    useEffect 실습

--- src/App.js
+++ src/App.js
@@ -1,77 +1,91 @@
-import { useState } from "react"
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import {useState} from "react"
+import {Navbar,Container,Nav,Button} from 'react-bootstrap';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import bg from './img/bg1.png';
-// import shoes01 from './img/shoes01.png';
-// import shoes02 from './img/shoes02.png';
-// import shoes03 from './img/shoes03.png';
 import data from './data.js';
-import { Routes, Route, useNavigate, Outlet } from 'react-router-dom'
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
 import Detail from "./routes/Detail";
+import axios from 'axios';
+
 
 function App() {
-  let [shoes] = useState(data);
-  let navigate = useNavigate();
+  let [shoes, setShoes] = useState(data);
+  let navigate = useNavigate();  
 
   return (
     <div className="App">
 
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">ShoesShop</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoesShop</Navbar.Brand>
           <Nav className="me-auto">
-            {/* <Nav.Link onClick={() => navigate(-1)}>Home</Nav.Link> */}
-            <Nav.Link onClick={() => navigate('/')}>Home</Nav.Link>
-            <Nav.Link onClick={() => navigate('/detail')}>Detail</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+            {/* <Nav.Link onClick={()=>{navigate(-1)}}>Home</Nav.Link> */}
+            <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>             
           </Nav>
         </Container>
       </Navbar>
       
       <Routes>
-         < Route path="/" element={
+         < Route path = "/" element={
           <>
            <div className="main-bg"></div>
            <div className = "container">
             <div className = "row">                   
              { shoes.map((a,i) => {
-               return < Card shoes={shoes[i]} i={i}></Card>
-              // <Card shoes={shoes[i]} images={images[i]}></Card>
-            })}
-        
-        </div> 
-      </div> 
-      </>
-     }/>
-    <Route path="/detail/:id" element={<Detail shoes={shoes}/>}/>
-    <Route path="/about" element={<About/>}>
-     <Route path="member" element={<h1>ㅁㅂ입니다</h1>}/>
-     <Route path="location" element={<h1>ㄹㅋㅇㅅ입니다</h1>}/>
-    </Route>
-    <Route path="*" element={<h1>없는 페이지입니다!!</h1>}/>
+               return < Card shoes={shoes[i]} i={i+1}></Card>              
+            })}        
+          </div> 
+          </div> 
+          <button onClick={()=>{
+            axios.get('https://codingapple1.github.io/shop/data2.json')
+            .then((결과)=>{
+              console.log(결과.data)
+              console.log(shoes)
+              let copy = [...shoes, ...결과.data]
+              setShoes(copy);
+            })
+            .catch(()=>{
+              console.log('접속실패')
+            })
+          }}>상품보기</button>
+
+         </>
+        }/>
+
+        {/* 여러장의 상세페이지를 만들기 */}
+      <Route path = "/detail/:id" element={<Detail shoes={shoes}/>}/>
+      <Route path = "/about" element={<About/>}>
+        <Route path = "member" element = {<div>member</div>}/>
+        <Route path = "location" element = {<div>location</div>}/>
+      </Route>
+      <Route path = "*" element={<div>페이지없음</div>}/> 
    </Routes>   
  </div>
  ); 
 }
 
-function About() {
-  return (
+function About(){
+  return(
     <div>
-      <h4>스포츠화 전문점</h4>
+      <Outlet></Outlet>
+      <h4>스포츠화전문점</h4>
       <Outlet></Outlet>
     </div>
-  );
+  )
 }
+
 
 function Card(props){
   return (
     <div className = "col-md-4">
       {/* <img src={props.images} alt='에어'/> */}
-      <img src={'https://raw.githubusercontent.com/hyennin/react-practice2-3/master/src/img/shoes0' + props.i + '.png' } width="100%" alt="에어"/>
+      <img src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg' } width="100%"/>
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.content}</p>
     </div>
   )
 }
+
 
 export default App;
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,66 +1,124 @@
-/*
-컴포넌트의 Life Cycle
-1. 페이지 로딩(mount)
-2. 업데이트(update)
-3. 제거(unmount)
-
-mount, update 시 코드 실행하려면
-useEffect() 사용
-*/
 
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Nav } from 'react-bootstrap';
 import styled from "styled-components";
 
-const YellowBtn = styled.button`
-    background: ${props => props.bg};
-    color: ${props => props.bg === 'blue' ? 'white' : 'black'};
-    padding: 10px;
-`;
+let YellowBtn = styled.button`
+ background : ${props => props.bg};
+ color : ${props => props.bg == 'blue' ?'white' : 'black'};
+ padding : 10px;
+`
+let NewBtn =  styled(YellowBtn)`  
+ color : ${props => props.bg == 'green' ?'white' : 'black'};
+`
+let Box = styled.div`
+ background : grey; 
+ padding : 20px;
+`
+function Detail(props) {
+  let [alert, setAlert] = useState(true)
+   // 유저가 입력한 URL파라미터의 값을 읽어오려면 useParams()이용 
+  let {id} = useParams(); 
+  // 3개의 탭의 값을 각각 0,1,2 정한 것임   
+  let [탭, 탭변경] = useState(0);
 
-const NewBtn = styled(YellowBtn)`
-    color: ${props => props.bg === 'green' ? 'white' : 'black'};
-`;
-
-const Box = styled.div`
-    background: gray;
-    padding: 20px;
-`;
-
-const Detail = (props) => {
-    let [ alert, setAlert ] = useState(true);
-    
-    useEffect(() => {
-        let a = setTimeout(() => setAlert(false), 2000);
-        return () => {
-            // 타이머 제거, useEffect가 실행되기 전에 실행됨
-            clearTimeout(a);
-        }
-    }, []) // [] : 첫 마운트 때에만 실행
-
-    // 유저가 입력한 URL 파라미터의 값을 읽어오기 위해 useParams()
-    let { id } = useParams();
-
-    return (
-        <div className="container">
-            { alert ? <div className="alert alert-waring">2초 이내 구매 시 할인</div> : '' }
-            <Box>
-                <YellowBtn bg="blue">스타일버튼_blue</YellowBtn>
-                <NewBtn bg="green">스타일버튼_green</NewBtn>
-            </Box>
-            <div className="row">
-                <div className="col-md-6">
-                    <img src={'https://raw.githubusercontent.com/hyennin/react-practice2-3/master/src/img/shoes0' + id + '.png' } width="100%" alt="에어"/>
+  useEffect(()=>{
+    let a = setTimeout(()=>{setAlert(false)}, 2000)  
+    console.log(2)
+    return ()=>{
+      console.log(1) 
+      // 타이머제거, useEffect가 실행되기 전에 실행됨 
+       clearTimeout(a)
+    }     
+  }, []) 
+  // [] mount시 1회가 코드가 실행되어라 
+  
+ 
+   return (
+       <div className="container">
+       {
+        alert == true   
+        ? <div className = "alert alert-waring">
+           2초이내 구매시 할인 
+         </div>
+        : null 
+       } 
+         <Box>
+         <YellowBtn bg="blue">스타일버튼_blue</YellowBtn>
+         <NewBtn bg="green">스타일버튼_green</NewBtn>
+         </Box>           
+           <div className="row">
+           <div className="col-md-6">
+             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />              
                 </div>
-                <div className="col-md-6">
-                    <h4 className="pt-5">{props.shoes[id].title}</h4>
-                    <p>{props.shoes[id].content}</p>
-                    <p>{props.shoes[id].price}</p>
-                    <button className="btn btn-danger">주문하기</button> 
+                 <div className="col-md-6">
+                     <h4 className="pt-5">{props.shoes[id].title}</h4>
+                     <p>{props.shoes[id].content}</p>
+                     <p>{props.shoes[id].price}원</p>
+                     <button className="btn btn-danger">주문하기</button> 
                 </div>
-            </div>
+             </div>
+             <Nav variant = "tabs" defaultActiveKey= "link0">
+                <Nav.Item>
+                  <Nav.Link onClick={()=>{탭변경(0)}} eventKey="link0">버튼0</Nav.Link>
+                </Nav.Item>  
+                <Nav.Item>
+                  <Nav.Link onClick={()=>{탭변경(1)}} eventKey="link1">버튼1</Nav.Link>
+                </Nav.Item> 
+                <Nav.Item>
+                  <Nav.Link onClick={()=>{탭변경(2)}} eventKey="link2">버튼2</Nav.Link>
+                </Nav.Item> 
+             </Nav>
+             <TabContent 탭={탭}/>
+
         </div> 
-    )
-}
+      )
+    }
+
+    // function TabContent(props){
+    //   if(props.탭 == 0){
+    //     return <div>내용0</div>
+    //   } 
+    //   if(props.탭 == 1){
+    //     return <div>내용1</div>
+    //   } 
+    //   if(props.탭 == 2){
+    //     return <div>내용2</div>
+    //   }
+    // }
+    
+    // function TabContent({탭}){
+    //   if(탭 == 0){
+    //     return <div>내용0</div>
+    //   } 
+    //   if(탭 == 1){
+    //     return <div>내용1</div>
+    //   } 
+    //   if(탭 == 2){
+    //     return <div>내용2</div>
+    //   }
+    
+    function TabContent({탭}){
+        const [fade, setFade] = useState('');
+
+        useEffect(() => {
+            // 탭이 변경될 때마다 fade의 값이 초기에는 공백이었다가
+            // 0.1초 뒤에 'end' 붙이기
+            const a = setTimeout(() => {setFade('end')}, 100)
+
+            return(() => {
+                clearTimeout(a);
+                setFade('');
+            })
+        }, [탭])
+
+        return (
+            <div className={"start " + fade}>
+                {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][탭]}
+            </div>
+        )
+    }
+
 
 export default Detail;
